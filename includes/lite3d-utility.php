af4faa401db25ec96ded8cfdf2a925afee146591
Alip Putra <beplas.studio@gmail.com>
2025-05-15 17:02:59 +0700

    add generate hash and shortcode format

--- includes/lite3d-utility.php
+++ includes/lite3d-utility.php
@@ -1,27 +1,75 @@
 <?php
 class LITE3D_Utility
 {
-  public static function count_assets($status)
+  private static function hash( $hash, $length = 7 ) {
+		return substr( $hash, 0, absint( $length ) );
+	}
+
+  public static function shortcode_format ( $post_id )
   {
-    global $wpdb;
-    $lite3d_table = LITE3D_TBL_ASSETS;
-
-    // Sanitize input
-    $status = sanitize_text_field($status);
-
-    // Prepare the SQL statement
-    $query = $wpdb->prepare("SELECT COUNT(*) FROM $lite3d_table WHERE status = %s", $status);
-    $get_assets_status = $wpdb->get_var($query);
-
-    return $get_assets_status;
+    $post = get_post_meta( $post_id, '_lite3d_viewer', true );
+    
+    if (isset($post['hash'])) {
+      return sprintf(
+        '[lite3d-viewer id="%1$s"]',
+        self::hash($post['hash'])
+      );
+    }
   }
 
-  public static function get_total_assets()
-  {
-      global $wpdb;
-      $lite3d_table = LITE3D_TBL_ASSETS;
+  public static function generate_hash($post_id) {
+    return sha1( implode( '|', array(
+      get_current_user_id(),
+      $post_id,
+      time(),
+      home_url(),
+    ) ) );
+  }
 
-      $lite3d_count = $wpdb->get_var("SELECT COUNT(*) FROM $lite3d_table");
-      return $lite3d_count;
+  public static function lite3d_format_atts( $atts )
+  {
+    $atts_filtered = array();
+  
+    foreach ( $atts as $name => $value ) {
+      $name = strtolower( trim( $name ) );
+  
+      if ( ! preg_match( '/^[a-z_:][a-z_:.0-9-]*$/', $name ) ) {
+        continue;
+      }
+  
+      static $boolean_attributes = array(
+        'checked',
+        'disabled',
+        'inert',
+        'multiple',
+        'readonly',
+        'required',
+        'selected',
+      );
+  
+      if ( in_array( $name, $boolean_attributes, true ) and '' === $value ) {
+        $value = false;
+      }
+  
+      if ( is_numeric( $value ) ) {
+        $value = (string) $value;
+      }
+  
+      if ( null === $value or false === $value ) {
+        unset( $atts_filtered[$name] );
+      } elseif ( true === $value ) {
+        $atts_filtered[$name] = $name; // boolean attribute
+      } elseif ( is_string( $value ) ) {
+        $atts_filtered[$name] = trim( $value );
+      }
+    }
+  
+    $output = '';
+  
+    foreach ( $atts_filtered as $name => $value ) {
+      $output .= sprintf( ' %1$s="%2$s"', $name, esc_attr( $value ) );
+    }
+  
+    return trim( $output );
   }
 }
