c513a8fe4d4434ad22e9bcbc775c4797185db8fa
Alip Putra <beplas.studio@gmail.com>
2025-05-16 20:02:27 +0700

    update admin init

---
+++ admin/lite3d-admin-init.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('WPINC')) {
+    die;
+}
+
+require_once LITE3D_ADMIN . 'lite3d-admin-menu.php';
+require_once LITE3D_ADMIN . 'lite3d-meta-box.php';
+require_once LITE3D_ADMIN . 'lite3d-settings.php';
+require_once LITE3D_ADMIN . 'lite3d-about.php';
